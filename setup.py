3ebcdc4872e2751b8d7757e474f22f523bfe177b
Lauren <49169837+laurenechan@users.noreply.github.com>
2021-08-19 10:44:08 -0500

    customizing for project

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
+#need to edit for my project
+# look at what Anne has in hers, will have to iterate to figure out what imports you need
+#keep looking for places that say project name all over
+
 import os
 import re
 
@@ -24,7 +28,7 @@ def find_version(*file_paths):
     raise RuntimeError('Unable to find version string.')
 
 
-__version__ = find_version('project_name', '__version__.py')
+__version__ = find_version('kg_envpolyreg', '__version__.py')
 
 test_deps = [
     'pytest',
@@ -40,13 +44,13 @@ extras = {
 }
 
 setup(
-    name='project_name',
+    name='kg_envpolyreg',
     version=__version__,
-    description='KG hub for project_name',
+    description='KG hub for kg_envpolyreg',
     long_description=long_description,
-    url='https://github.com/Knowledge-Graph-Hub/project_name',
-    author='Harshad Hegde',
-    author_email='hhegde@lbl.gov',
+    url='https://github.com/Knowledge-Graph-Hub/kg_envpolyreg',
+    author='Lauren Chan',
+    author_email='chanl@oregonstate.edu',
     python_requires='>=3.7',
 
     # choose your license
